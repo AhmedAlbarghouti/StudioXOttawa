dbdbe962fd3574e48fed44176a7502c0f8fa9751
xiaox <qin00029@alginquinlive.com>
2021-02-02 17:20:30 -0500

    About us page by Xiaoxi Qin

---
+++ app/src/main/java/com/example/studioxottawa/AboutusActivity.java
@@ -0,0 +1,40 @@
+package com.example.studioxottawa;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class AboutusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+
+//        final TextView textView = findViewById(R.id.textView1);
+//
+//        String str = "We are OPEN! Classes only available online during the lockdown. \n" +
+//                "*Hybrid program (in-person and online simultaneously) will be back after the lockdown period. \n" +
+//                "See Interactive schedule below or contact the Studio by e-mail for more details! \n\n " +
+//
+//                "Welcome to Studio X Ottawa Inc. YOUR “one stop” eclectic dance, fitness and space rental Centre! \n\n\n" +
+//                "Office Hours\n" +
+//                "\n" +
+//                "Office hours\n" +
+//                "For the summer, our office will only be open during classes hours.\n" +
+//                "\n" +
+//                "Business Hours\n" +
+//                "Monday to Thursdays: classes hours\n" +
+//                "Friday: Closed\n" +
+//                "Saturday: Clossed\n" +
+//                "Sunday: Classes hours (except on long weekends)";
+//        textView.setText(str);
+
+
+
+
+
+    }
+}
--- app/src/main/java/com/example/studioxottawa/MainActivity.java
+++ app/src/main/java/com/example/studioxottawa/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.studioxottawa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button aboutusBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get the reference of Button's
+        aboutusBtn = (Button) findViewById(R.id.aboutusButton);  //xiaoxi
+
+// perform setOnClickListener event on aboutus btn //xiaoxi  {
+        aboutusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //load First Fragment
+//                loadFragment(new FragmentAboutus());
+                loadAboutusActivity();
+            }
+
+
+        });  //xiaoxi }
     }
-}
+
+    //xiaoxi {
+    private void loadAboutusActivity() {
+
+        Intent myIntent = new Intent(MainActivity.this, AboutusActivity.class);
+        MainActivity.this.startActivity(myIntent);
+
+    }   //xiaoxi }
+
+    }
