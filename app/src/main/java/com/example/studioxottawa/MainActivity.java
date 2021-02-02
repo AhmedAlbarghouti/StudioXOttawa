770f4bdb14586a04dab5256baab08e2c32f3802d
Lubin-Hans <lubi0034@algonquinlive.com>
2021-02-02 09:01:02 -0500

    Contact button and contact us page-Initial commit

--- app/src/main/java/com/example/studioxottawa/MainActivity.java
+++ app/src/main/java/com/example/studioxottawa/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.studioxottawa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button contactbtn=findViewById(R.id.contactButton);
+        contactbtn.setOnClickListener(btn->{
+            Intent contact=new Intent(this,contact.class);
+            startActivity(contact);
+        });
+
     }
 }
