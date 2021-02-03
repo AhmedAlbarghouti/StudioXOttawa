0a2dac5b8c22a8560d022625e2b0f766a5a8a8b3
BigG13 <ethangumazda@gmail.com>
2021-02-02 19:09:02 -0500

    feb02

--- app/src/main/java/com/example/studioxottawa/news/NewsActivity.java
+++ app/src/main/java/com/example/studioxottawa/news/NewsActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
+import com.example.studioxottawa.R;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Intent;
@@ -85,116 +85,59 @@ public class NewsActivity extends AppCompatActivity{
         myList = findViewById(R.id.newsListView);
         myList.setAdapter( myAdapter = new MyListAdapter());
 
-        boolean isTablet = findViewById(R.id.frameLayout) != null;
+//        boolean isTablet = findViewById(R.id.frameLayout) != null;
 
-        //User can use long click to add news to favourite list
-        myList.setOnItemLongClickListener((parent, view, position, id) -> {
-            new AlertDialog.Builder(BbcActivity.this)
-                    .setTitle(getString(R.string.BBC_AlertDialogTitle))
-                    .setMessage(getString(R.string.BBC_rowNum) + " " + position + "\n" + getString(R.string.BBC_databaseId) + " " + id)
-                    .setNegativeButton(getString(R.string.BBC_Favourite_NegativeButton),null)
-                    .setPositiveButton(getString(R.string.BBC_Favourite_PositiveButton), (click, arg) -> {
 
-                        //if confirmed, save the news to database as well
-                        ContentValues newRowValues = new ContentValues();
-                        newRowValues.put(MyOpener.COL_TITLE, elements.get(position).getTitle());
-                        newRowValues.put(MyOpener.COL_DESCRIPTION, elements.get(position).getDescription());
-                        newRowValues.put(MyOpener.COL_LINK, elements.get(position).getLink());
-                        newRowValues.put(MyOpener.COL_DATE, elements.get(position).getDate());
 
-                        //Check if the news already exist in the Favourite List
-                        //Load favourite list from database into tempFavourite
-                        loadDataFromDatabase();
-                        boolean isExist = false;
-                        for(News tempNews:tempFavourite){
-                            if(elements.get(position).getTitle().equals(tempNews.getTitle())) isExist=true;
-                        }
-                        if(isExist == false) {
-                            long newId = db.insert(MyOpener.TABLE_NAME, null, newRowValues);
-                        }else{
-                            Toast.makeText(this, "Cannot add, this news already exist in the favourite list", Toast.LENGTH_SHORT).show();
-                        }
-                        myAdapter.notifyDataSetChanged();
-                    }).create().show();
-            return true;   });
-
-        Button favouriteButton = (Button)findViewById(R.id.GoToFavourite);
-        favouriteButton.setOnClickListener( new View.OnClickListener()
-        {  public void onClick(View v){
-            Intent goToChatRoom = new Intent(BbcActivity.this, FavourityActivity.class);
-            startActivity(goToChatRoom);
-        } });
+//        Button favouriteButton = (Button)findViewById(R.id.GoToFavourite);
+//        favouriteButton.setOnClickListener( new View.OnClickListener()
+//        {  public void onClick(View v){
+//            Intent goToChatRoom = new Intent(BbcActivity.this, FavourityActivity.class);
+//            startActivity(goToChatRoom);
+//        } });
 
 
         //create the fragment of news
-        myList.setOnItemClickListener((list, item, position, id) -> {
-            //Create a bundle to pass data to the new fragment
-            Bundle dataToPass = new Bundle();
-            dataToPass.putString(NEWS_TITLE, elements.get(position).getTitle() );
-            dataToPass.putString(NEWS_DESCRIPTION, elements.get(position).getDescription() );
-            dataToPass.putString(NEWS_LINK, elements.get(position).getLink() );
-            dataToPass.putString(NEWS_DATE, elements.get(position).getDate() );
+//        myList.setOnItemClickListener((list, item, position, id) -> {
+//            //Create a bundle to pass data to the new fragment
+//            Bundle dataToPass = new Bundle();
+//            dataToPass.putString(NEWS_TITLE, elements.get(position).getTitle() );
+//            dataToPass.putString(NEWS_DESCRIPTION, elements.get(position).getDescription() );
+//            dataToPass.putString(NEWS_LINK, elements.get(position).getLink() );
+//            dataToPass.putString(NEWS_DATE, elements.get(position).getDate() );
+//
+//            dataToPass.putInt(NEWS_POSITION, position);
+//            dataToPass.putLong(NEWS_ID, id);
+//
+//            if(isTablet)
+//            {
+//                dFragment = new DetailsFragment();
+//                dFragment.setArguments( dataToPass );
+//                getSupportFragmentManager()
+//                        .beginTransaction()
+//                        .replace(R.id.frameLayout, dFragment)
+//                        .commit();
+//            }
+//            else //isPhone
+//            {
+//                Intent nextActivity = new Intent(BbcActivity.this, EmptyActivity.class);
+//                nextActivity.putExtras(dataToPass); //send data to next activity
+//                startActivity(nextActivity); //make the transition
+//            }
+//        });
 
-            dataToPass.putInt(NEWS_POSITION, position);
-            dataToPass.putLong(NEWS_ID, id);
-
-            if(isTablet)
-            {
-                dFragment = new DetailsFragment();
-                dFragment.setArguments( dataToPass );
-                getSupportFragmentManager()
-                        .beginTransaction()
-                        .replace(R.id.frameLayout, dFragment)
-                        .commit();
-            }
-            else //isPhone
-            {
-                Intent nextActivity = new Intent(BbcActivity.this, EmptyActivity.class);
-                nextActivity.putExtras(dataToPass); //send data to next activity
-                startActivity(nextActivity); //make the transition
-            }
-        });
-
-        //Toolbar and NavigationDraw
-        Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(tBar);
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
-                drawer, tBar, R.string.BBC_open, R.string.BBC_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    /**
-     * load data from database
-     */
-    private void loadDataFromDatabase()
-    {
-        dbOpener = new MyOpener(this);
-        db = dbOpener.getWritableDatabase();
-        String [] columns = {MyOpener.COL_ID, MyOpener.COL_TITLE, MyOpener.COL_DESCRIPTION, MyOpener.COL_LINK, MyOpener.COL_DATE};
-        Cursor myCursor = db.query(false, MyOpener.TABLE_NAME, columns, null, null, null, null, null, null);
-
-        int titleColumnIndex = myCursor.getColumnIndex(MyOpener.COL_TITLE);
-        int descriptionColumnIndex = myCursor.getColumnIndex(MyOpener.COL_DESCRIPTION);
-        int linkColumnIndex = myCursor.getColumnIndex(MyOpener.COL_LINK);
-        int dateColumnIndex = myCursor.getColumnIndex(MyOpener.COL_DATE);
-        int idColIndex = myCursor.getColumnIndex(MyOpener.COL_ID);
-
-        while(myCursor.moveToNext())
-        {
-            String title = myCursor.getString(titleColumnIndex);
-            String description = myCursor.getString(descriptionColumnIndex);
-            String link = myCursor.getString(linkColumnIndex);
-            String date = myCursor.getString(dateColumnIndex);
-            long id = myCursor.getLong(idColIndex);
-            //add the news to the array list:
-            tempFavourite.add(new News(title, description, link, date));
-        }
+//        //Toolbar and NavigationDraw
+//        Toolbar tBar = (Toolbar)findViewById(R.id.toolbar);
+//        setSupportActionBar(tBar);
+//
+//        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
+//                drawer, tBar, R.string.BBC_open, R.string.BBC_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+//
+//        NavigationView navigationView = findViewById(R.id.nav_view);
+//        navigationView.setNavigationItemSelectedListener(this);
     }
 
     /**
@@ -328,103 +271,103 @@ public class NewsActivity extends AppCompatActivity{
         }
     }
 
-    /**
-     * inflates the menu from XML layout
-     * @param menu - {@link Menu} object that is applied by the activity
-     * @return true
-     */
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu items for use in the action bar
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu3, menu);
+//    /**
+//     * inflates the menu from XML layout
+//     * @param menu - {@link Menu} object that is applied by the activity
+//     * @return true
+//     */
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu items for use in the action bar
+//        MenuInflater inflater = getMenuInflater();
+//        inflater.inflate(R.menu.menu3, menu);
+//
+//        return true;
+//    }
 
-        return true;
-    }
+//    /**
+//     * @param item - {@link MenuItem} object in the {@link Menu}
+//     * @return true
+//     */
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        String message = null;
+//        Intent goTo;
+//        //Look at your menu XML file. Put a case for every id in that file:
+//        switch(item.getItemId())
+//        {
+//            //what to do when the menu item is selected:
+//            case R.id.bbc_fav_pic:
+//                message = getResources().getString(R.string.BBC_Toolbar_Option)
+//                        +" "+getResources().getString(R.string.BBC_Favourite);
+//                goTo = new Intent(BbcActivity.this, FavourityActivity.class);
+//                startActivity(goTo);
+//                break;
+//            case R.id.guardian_pic:
+//                message = getResources().getString(R.string.BBC_Toolbar_Option)
+//                        +" "+getResources().getString(R.string.BBC_Guardian_news);
+//                goTo = new Intent(BbcActivity.this, GuardianMain.class);
+//                startActivity(goTo);
+//                break;
+//            case R.id.nasa_pic:
+//                message = getResources().getString(R.string.BBC_Toolbar_Option)
+//                        +" "+getResources().getString(R.string.BBC_NASA);
+//                goTo = new Intent(BbcActivity.this, ImageDay.class);
+//                startActivity(goTo);
+//                break;
+//            case R.id.nasa_earth_pic:
+//                message = getResources().getString(R.string.BBC_Toolbar_Option)
+//                        +" "+getResources().getString(R.string.BBC_NASA_earth);
+//                goTo = new Intent(BbcActivity.this, BingMain.class);
+//                startActivity(goTo);
+//                break;
+//            case R.id.bbc_help:
+//                message = getResources().getString(R.string.BBC_Toolbar_Option)
+//                        +" "+getResources().getString(R.string.BBC_help);
+//                new AlertDialog.Builder(BbcActivity.this)
+//                        .setTitle(getString(R.string.BBC_AlertDialogTitle3))
+//                        .setMessage(getString(R.string.BBC_HelpMes1) + "\n" + getString(R.string.BBC_HelpMes2) + "\n"
+//                                + getString(R.string.BBC_HelpMes3) + "\n" + getString(R.string.BBC_HelpMes4) + "\n"
+//                                + getString(R.string.BBC_HelpMes5) + "\n")
+//                        .setNegativeButton(getString(R.string.BBC_Help_NegativeButton),null)
+//                        .setPositiveButton(getString(R.string.BBC_Help_PositiveButton), null).show();
+//                break;
+//        }
+//        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+//        return true;
+//    }
 
-    /**
-     * @param item - {@link MenuItem} object in the {@link Menu}
-     * @return true
-     */
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        String message = null;
-        Intent goTo;
-        //Look at your menu XML file. Put a case for every id in that file:
-        switch(item.getItemId())
-        {
-            //what to do when the menu item is selected:
-            case R.id.bbc_fav_pic:
-                message = getResources().getString(R.string.BBC_Toolbar_Option)
-                        +" "+getResources().getString(R.string.BBC_Favourite);
-                goTo = new Intent(BbcActivity.this, FavourityActivity.class);
-                startActivity(goTo);
-                break;
-            case R.id.guardian_pic:
-                message = getResources().getString(R.string.BBC_Toolbar_Option)
-                        +" "+getResources().getString(R.string.BBC_Guardian_news);
-                goTo = new Intent(BbcActivity.this, GuardianMain.class);
-                startActivity(goTo);
-                break;
-            case R.id.nasa_pic:
-                message = getResources().getString(R.string.BBC_Toolbar_Option)
-                        +" "+getResources().getString(R.string.BBC_NASA);
-                goTo = new Intent(BbcActivity.this, ImageDay.class);
-                startActivity(goTo);
-                break;
-            case R.id.nasa_earth_pic:
-                message = getResources().getString(R.string.BBC_Toolbar_Option)
-                        +" "+getResources().getString(R.string.BBC_NASA_earth);
-                goTo = new Intent(BbcActivity.this, BingMain.class);
-                startActivity(goTo);
-                break;
-            case R.id.bbc_help:
-                message = getResources().getString(R.string.BBC_Toolbar_Option)
-                        +" "+getResources().getString(R.string.BBC_help);
-                new AlertDialog.Builder(BbcActivity.this)
-                        .setTitle(getString(R.string.BBC_AlertDialogTitle3))
-                        .setMessage(getString(R.string.BBC_HelpMes1) + "\n" + getString(R.string.BBC_HelpMes2) + "\n"
-                                + getString(R.string.BBC_HelpMes3) + "\n" + getString(R.string.BBC_HelpMes4) + "\n"
-                                + getString(R.string.BBC_HelpMes5) + "\n")
-                        .setNegativeButton(getString(R.string.BBC_Help_NegativeButton),null)
-                        .setPositiveButton(getString(R.string.BBC_Help_PositiveButton), null).show();
-                break;
-        }
-        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
-        return true;
-    }
-
-    /**
-     * @param item - {@link MenuItem} object in the NavigationDrawer
-     * @return false
-     */
-    @Override
-    public boolean onNavigationItemSelected( MenuItem item) {
-
-        String message = null;
-
-        int id = item.getItemId();
-        Intent goTo;
-
-        if(id == R.id.goToFav){
-
-            goTo = new Intent(BbcActivity.this, FavourityActivity.class);
-            startActivity(goTo);
-        }else if(id == R.id.goToGardian){
-            goTo = new Intent(BbcActivity.this, GuardianMain.class);
-            startActivity(goTo);
-        }else if(id == R.id.goToNASA){
-
-            goTo = new Intent(BbcActivity.this, ImageDay.class);
-            startActivity(goTo);
-        }else if(id == R.id.goToNASAEarth){
-
-            goTo = new Intent(BbcActivity.this, BingMain.class);
-            startActivity(goTo);
-        }
-        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
-        drawerLayout.closeDrawer(GravityCompat.START);
-        return false;
-    }
+//    /**
+//     * @param item - {@link MenuItem} object in the NavigationDrawer
+//     * @return false
+//     */
+//    @Override
+//    public boolean onNavigationItemSelected( MenuItem item) {
+//
+//        String message = null;
+//
+//        int id = item.getItemId();
+//        Intent goTo;
+//
+//        if(id == R.id.goToFav){
+//
+//            goTo = new Intent(BbcActivity.this, FavourityActivity.class);
+//            startActivity(goTo);
+//        }else if(id == R.id.goToGardian){
+//            goTo = new Intent(BbcActivity.this, GuardianMain.class);
+//            startActivity(goTo);
+//        }else if(id == R.id.goToNASA){
+//
+//            goTo = new Intent(BbcActivity.this, ImageDay.class);
+//            startActivity(goTo);
+//        }else if(id == R.id.goToNASAEarth){
+//
+//            goTo = new Intent(BbcActivity.this, BingMain.class);
+//            startActivity(goTo);
+//        }
+//        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+//        drawerLayout.closeDrawer(GravityCompat.START);
+//        return false;
+//    }
 }
 
