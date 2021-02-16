fb1379d61b0c95a4783e7896e3901c51b7287fe4
Steven Dancer <danc0026@algonquinlive.com>
2021-02-16 10:31:22 -0500

    VOD library population and playback proof-of-concept completed.
    Signed-off-by: Steven Dancer <danc0026@algonquinlive.com>

--- app/src/main/java/com/example/studioxottawa/MainActivity.java
+++ app/src/main/java/com/example/studioxottawa/MainActivity.java
@@ -15,6 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Button vodButton = findViewById(R.id.vodsButton);
+        vodButton.setOnClickListener(v-> {
+            loadVodLibrary();
+        });
         // get the reference of Button's
         aboutusBtn = (Button) findViewById(R.id.aboutusButton);  //xiaoxi
 
@@ -26,6 +30,23 @@ public class MainActivity extends AppCompatActivity {
                 loadAboutusActivity();
             }
         });  //xiaoxi }
+    }
+
+    private void loadVodLibrary() {
+        Intent vodLibrary = new Intent(MainActivity.this, VODLibraryActivity.class);
+        //This utilizes API calls to the youtube Data v3 interface.
+        // Avoid unnecessary spamming in order to preserve api call quotas during testing.
+        //Use the included loadVodDebug below if testing Vod playback.
+        startActivity(vodLibrary);
+        //loadVodDebug();
+    }
+
+    private void loadVodDebug() {
+        //Debug method to bypass library to save on api calls.  Uncomment the loadVodDebug(); line in loadVodLibrary to enable.
+        String url = "https://r4---sn-ux3n588t-mjh6.googlevideo.com/videoplayback?expire=1613507657&ei=6dcrYKbFLKyL2_gPwK-j0As&ip=76.10.173.120&id=o-AOJB_WbF3NyGnjMjgrAveJZxzv1KHtFS212fW5W_vMH2&itag=22&source=youtube&requiressl=yes&mh=_8&mm=31%2C29&mn=sn-ux3n588t-mjh6%2Csn-tt1e7n7l&ms=au%2Crdu&mv=m&mvi=4&pl=21&initcwndbps=1153750&vprv=1&mime=video%2Fmp4&ns=QyuuKWKpdjLyaiS0O5bkIYgF&ratebypass=yes&dur=81.130&lmt=1411161817663260&mt=1613485724&fvip=6&c=WEB&n=eEv0tFPOg_deRiChZH&sparams=expire%2Cei%2Cip%2Cid%2Citag%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cratebypass%2Cdur%2Clmt&sig=AOq0QJ8wRQIgNV2m5tKVBy2PyYhCGIUMNm8p1B9iild0q0elqVnjYLYCIQDrY5Tlv4E7Ua_CHJb5PRX6bXxZt8VseQUoV9hmHdKObw%3D%3D&lsparams=mh%2Cmm%2Cmn%2Cms%2Cmv%2Cmvi%2Cpl%2Cinitcwndbps&lsig=AG3C_xAwRQIgG9wOnXOENa2DcbBYnap_t_UDU_GoR_YWH9Z_xKkP0VQCIQDcpY021gCadZuYkwZzHYROdXh9seokvbCVIrrTcTV1xQ%3D%3D";
+        Intent vodPlayback = new Intent(MainActivity.this, VODActivity.class);
+        vodPlayback.putExtra("playbackURI", url);
+        startActivity(vodPlayback);
     }
 
     //xiaoxi {
