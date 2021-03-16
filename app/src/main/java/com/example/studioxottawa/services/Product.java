8c24283e72deb0a18fe396eeeb1ab2aa457c7269
Lubin-Hans <lubi0034@algonquinlive.com>
2021-03-16 04:09:39 -0400

    -Firebase Implementation -New images -Cart Activity Added

--- app/src/main/java/com/example/studioxottawa/services/Product.java
+++ app/src/main/java/com/example/studioxottawa/services/Product.java
@@ -1,17 +1,30 @@
 package com.example.studioxottawa.services;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
 
 public class Product {
-    Bitmap thumbnail;
+    String thumbnail;
     String item;
     Double price;
+    int quantity;
 
-    public Bitmap getThumbnail() {
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getThumbnail() {
         return thumbnail;
     }
 
-    public void setThumbnail(Bitmap thumbnail) {
+    public void setThumbnail(String thumbnail) {
         this.thumbnail = thumbnail;
     }
 
@@ -31,9 +44,30 @@ public class Product {
         this.price = price;
     }
 
-    public Product(Bitmap thumbnail, String item, Double price) {
+    public String BitMapToString(Bitmap bitmap){
+        ByteArrayOutputStream baos=new  ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100, baos);
+        byte [] b=baos.toByteArray();
+        String temp= Base64.encodeToString(b, Base64.DEFAULT);
+        return temp;
+    }
+
+    public Bitmap getBitmap(){
+        try{
+            byte [] encodeByte = Base64.decode(getThumbnail(), Base64.DEFAULT);
+            Bitmap bitmap = BitmapFactory.decodeByteArray(encodeByte, 0, encodeByte.length);
+            return bitmap;
+        }
+        catch(Exception e){
+            e.getMessage();
+            return null;
+        }
+    }
+
+    public Product(String thumbnail, String item, Double price,int quantity) {
         setThumbnail(thumbnail);
         setItem(item);
         setPrice(price);
+        setQuantity(quantity);
     }
 }
