3fa17d4cff1234ac7ac055e237a2d41ad3678349
Ahmed Albarghouti <ahmedalbarghou@gmail.com>
2021-04-07 23:07:16 -0400

    Work in progress. (News integrated with bottom navigation fragments)

--- app/src/main/java/com/example/studioxottawa/welcome/WelcomePage.java
+++ app/src/main/java/com/example/studioxottawa/welcome/WelcomePage.java
@@ -7,16 +7,20 @@ import android.os.Bundle;
 import android.os.Handler;
 
 import com.example.studioxottawa.R;
+import com.example.studioxottawa.news.NewsFragment;
+import com.example.studioxottawa.schedule.ScheduleFragment;
 
 public class WelcomePage extends AppCompatActivity {
-
-        private static int SPLASH_TIME_OUT = 4000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
-
+//        ScheduleFragment sobj = new ScheduleFragment();
+        NewsFragment nobj = new NewsFragment();
+//        sobj.loadEvents();
+        nobj.loadNews();
+        int SPLASH_TIME_OUT = 4000;
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
@@ -24,7 +28,7 @@ public class WelcomePage extends AppCompatActivity {
                 startActivity(homeIntent);
                 finish();
             }
-        },SPLASH_TIME_OUT);
+        }, SPLASH_TIME_OUT);
 
 
     }
