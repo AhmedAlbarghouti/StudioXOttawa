a34a3cf226741fb74bed583ed9eee35fd5412721
Ahmed Albarghouti <ahmedalbarghou@gmail.com>
2021-04-09 21:38:04 -0400

    Work in progress.

---
+++ app/src/main/java/com/example/studioxottawa/welcome/AccountFragment.java
@@ -0,0 +1,16 @@
+package com.example.studioxottawa.welcome;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.studioxottawa.R;
+
+public class AccountFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_account_fragment);
+    }
+}
