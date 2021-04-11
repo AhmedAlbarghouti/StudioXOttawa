a3e23c19bfe33d3f8cdcfe1ee1307dfbbfd3dff7
Ahmed Albarghouti <ahmedalbarghou@gmail.com>
2021-04-10 22:14:57 -0400

    Integrated About us, contact us , admin portal, signout

--- app/src/main/java/com/example/studioxottawa/welcome/MoreFragment.java
+++ app/src/main/java/com/example/studioxottawa/welcome/MoreFragment.java
@@ -16,14 +16,23 @@ import com.example.studioxottawa.aboutus.AboutusActivity;
 import com.example.studioxottawa.contact.ContactUs;
 import com.example.studioxottawa.staff.StaffMenu;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MoreFragment extends Fragment {
 
     private Button accountInfoBtn,eventsBtn,productsBtn,contactBtn,aboutBtn,signOutBtn,adminBtn;
+    private FirebaseUser user;
+    private DatabaseReference reference;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_account_fragment,container,false);
+        ViewGroup root = (ViewGroup) inflater.inflate(R.layout.activity_more_fragment,container,false);
 
         accountInfoBtn = root.findViewById(R.id.account_info_button);
         eventsBtn = root.findViewById(R.id.events_booked_button);
@@ -32,6 +41,24 @@ public class MoreFragment extends Fragment {
         aboutBtn = root.findViewById(R.id.about_us_button);
         signOutBtn = root.findViewById(R.id.logout_button);
         adminBtn = root.findViewById(R.id.admin_portal_button);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        String userID = user.getUid();
+        reference.child(userID).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                User signInUser = snapshot.getValue(User.class);
+                Boolean isStaff = signInUser.staff;
+                if (isStaff) {
+                    adminBtn.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
 
         aboutBtn.setOnClickListener(click -> {
             goToAboutUs();
